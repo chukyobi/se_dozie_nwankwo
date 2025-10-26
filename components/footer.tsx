e8e8650629f5f0b7ed4b18ceec9458a1c185346f
Chukwudi Obi <162724360+chukyobi@users.noreply.github.com>
2025-10-26 13:01:12 -0700

    Change footer background color from gray-800 to gray-900

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import { Mail, Facebook, Twitter, Instagram, Linkedin } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 py-20 text-white">
+    <footer className="bg-gray-900 py-20 text-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Wrapper grid */}
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mb-12">
